02cbeb02218e13214c6a7e4a1abfecc2169d0dde
Bijan <bijanbwb@gmail.com>
2015-11-25 19:51:54 -0500

    Add comments

--- src/try_clojure/core.clj
+++ src/try_clojure/core.clj
@@ -1,11 +1,14 @@
+; Namespace
 (ns try-clojure.core
   (:use compojure.core)
   (:require
     [ring.adapter.jetty :as jetty]))
 
+; Routing
 (defroutes main-routes
   (GET "/" [] "Hello World"))
 
+; Main
 (defn -main []
   (jetty/run-jetty main-routes {:port 5000}))
 
