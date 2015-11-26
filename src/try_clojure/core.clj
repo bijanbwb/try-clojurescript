9a62519db91ab058b23010e6f66423af90c00007
Bijan <bijanbwb@gmail.com>
2015-11-25 19:59:37 -0500

    Render template for root path

--- src/try_clojure/core.clj
+++ src/try_clojure/core.clj
@@ -2,11 +2,24 @@
 (ns try-clojure.core
   (:use compojure.core)
   (:require
-    [ring.adapter.jetty :as jetty]))
+    [ring.adapter.jetty :as jetty]
+    [clostache.parser :as clostache]))
+
+; Templates
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource
+    (str "templates/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+  (clostache/render (read-template template-file) params))
+
+; Views
+(defn index []
+  (render-template "index" {:greeting "Greetings"}))
 
 ; Routing
 (defroutes main-routes
-  (GET "/" [] "Hello World"))
+  (GET "/" [] (index)))
 
 ; Main
 (defn -main []
