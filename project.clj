d73f9997152f2dfc1017cf90f42d8ed80089329c
Bijan <bijanbwb@gmail.com>
2015-11-25 20:08:20 -0500

    Update app name for Heroku

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject try-clojure "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "Try Clojure"
-  :url "http://try-clojure.herokuapp.com"
+  :url "http://try-clojurescript.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
