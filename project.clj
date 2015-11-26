7b511e0047a4a81d99c7da70d2f8a171eed7f4a8
Bijan <bijanbwb@gmail.com>
2015-11-25 19:45:45 -0500

    Configure project.clj and dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject try-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :description "Try Clojure"
+  :url "http://try-clojure.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5"]
+                 [de.ubercode.clostache/clostache "1.3.1"]]
+  :main try-clojure.core)
