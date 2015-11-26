511b471af7884d674246220115acccdf2a1b05ae
Bijan <bijanbwb@gmail.com>
2015-11-25 19:51:28 -0500

    Setup routing

--- src/try_clojure/core.clj
+++ src/try_clojure/core.clj
@@ -1,8 +1,11 @@
 (ns try-clojure.core
+  (:use compojure.core)
   (:require
     [ring.adapter.jetty :as jetty]))
 
+(defroutes main-routes
+  (GET "/" [] "Hello World"))
+
 (defn -main []
-  (jetty/run-jetty
-    (fn [req] {:status 200 :body "Hello World"})
-    {:port 5000}))
+  (jetty/run-jetty main-routes {:port 5000}))
+
